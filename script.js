0df01129196656122686aa7e977c8d012bd63c71
aditya-bhakat <bhakataditya0@gmail.com>
2025-07-08 12:30:32 +0530

    script issue

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ import { getDatabase, ref, push, update, remove, onValue } from "https://www.gst
 
 // Your Firebase Config (replace with yours)
 const firebaseConfig = {
-    apiKey: "AIzaSyDS7bT4oLjH7IS9en9n6E6UrUJeJ3OAPHA",
-    authDomain: "store-management-anshu.firebaseapp.com",
-    databaseURL: "https://store-management-anshu-default-rtdb.firebaseio.com",
-    projectId: "store-management-anshu",
-    storageBucket: "store-management-anshu.appspot.com",
-    messagingSenderId: "858551373452",
-    appId: "1:858551373452:web:62cfa0056901a1bd3af5a9"
+  apiKey: "AIzaSyDS7bT4oLjH7IS9en9n6E6UrUJeJ3OAPHA",
+  authDomain: "store-management-anshu.firebaseapp.com",
+  databaseURL: "https://store-management-anshu-default-rtdb.firebaseio.com",
+  projectId: "store-management-anshu",
+  storageBucket: "store-management-anshu.appspot.com",
+  messagingSenderId: "858551373452",
+  appId: "1:858551373452:web:62cfa0056901a1bd3af5a9"
 };
 
 // Initialize Firebase
